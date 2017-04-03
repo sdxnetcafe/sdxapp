6a6c7294dbc9d7e2c9bbd213d4debecacf2a673d
AndyScherzinger <info@andy-scherzinger.de>
2017-04-03 21:37:00 +0200

    add icons to the grid headers depending on the type

--- src/main/java/com/owncloud/android/ui/activity/FolderSyncActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FolderSyncActivity.java
@@ -292,7 +292,8 @@ public class FolderSyncActivity extends FileActivity implements FolderSyncAdapte
                 syncedFolder.isEnabled(),
                 mediaFolder.filePaths,
                 mediaFolder.folderName,
-                mediaFolder.numberOfFiles);
+                mediaFolder.numberOfFiles,
+                mediaFolder.type);
     }
 
     /**
@@ -315,7 +316,8 @@ public class FolderSyncActivity extends FileActivity implements FolderSyncAdapte
                 false,
                 mediaFolder.filePaths,
                 mediaFolder.folderName,
-                mediaFolder.numberOfFiles);
+                mediaFolder.numberOfFiles,
+                mediaFolder.type);
     }
 
     /**
